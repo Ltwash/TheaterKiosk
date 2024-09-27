4f3a33a100eda3210a15bed9031393b15c7f93a9
lukeh <hensonlt@mail.uc.edu>
2024-09-27 11:22:35 -0400

    Task 4 completed

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("Please enter your age");
+    int age = in.nextInt();
+
+    if (age >= 21) {
+        System.out.println("You get a paper wristband");
+    }
+else if (age < 21) {
+
+    }
+else{
+        System.out.println("Please enter a valid number");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
